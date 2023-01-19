56fac3df3ed9ff00a9e0228c63cfb5bffb62c67f
Pedro L <pedrocontact22@gmail.com>
2023-01-19 15:01:55 -0300

    Add section 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import HeroCarousel from "@/components/HeroCarousel";
 import Head from "next/head";
 import Image from "next/image";
 import Section2Image from "../images/blue-living-room-accent-wall.jpeg";
+import Section3Image from "../images/gray-living-room-accent-wall.jpeg";
 
 export default function Home() {
   return (
@@ -49,6 +50,27 @@ export default function Home() {
                 des résultats impeccables et incroyables.
               </p>
             </div>
+          </div>
+        </section>
+        <section className="bg-[#494947] p-6 sm:py-16 sm:px-28 md:py-20 md:px-32">
+          <div className="mx-auto lg:flex lg:gap-10 w-80 sm:w-[28rem] md:w-[36rem] lg:w-[55rem]">
+            <div className="text-center-bug lg:text-right text-white">
+              <h2 className="lg:pt-10 font-bold text-base sm:text-lg md:text-xl">
+                MISSION
+              </h2>
+              <p className="text-sm sm:text-base md:text-lg">
+                Offrir la meilleure expérience visuelle que les gens souhaitent
+                dans leur environnement, en les rendant beaux, propres et
+                agréables. Pour cela, nous comptons sur la créativité, la
+                concentration et la détermination qui, selon nous, sont des
+                éléments essentiels pour répondre et dépasser les attentes.
+              </p>
+            </div>
+            <Image
+              src={Section3Image}
+              alt="blue living room accent wall"
+              className="object-cover h-48 sm:h-56 md:h-72 lg:h-80 lg:w-[27rem] border-4 border-white"
+            />
           </div>
         </section>
       </main>
